6edcac6f18038387dc14eae731b4038ef1cf605c
Henrique Rocha <81716008+Rochadevj@users.noreply.github.com>
2024-01-30 23:27:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 function toggleMode () {
 const html = document.documentElement
 html.classList.toggle('light')
-
-/* if(html.classList.contains('light')) {
-html.classList.remove('light')
-} else {
-  html.classList.add('light')
-} */
 }
 
 setInterval(() => {
